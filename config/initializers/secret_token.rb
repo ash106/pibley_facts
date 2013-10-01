d07509d9c98fb700db0b6a21fab8942423ba3a35
Alex Howington <alex.howington@gmail.com>
2013-09-30 20:46:59 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PibleyFacts::Application.config.secret_token = '81e966995ede0515adc671f7e3452566bb74e20bccf88660e630f88b33fd2bf869b0542a69223b651d48d9aaf3e583949aa62c67f65b873c919e23a12938738c'
